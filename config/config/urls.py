081255a1aef03dfdd588087bf69f7380713fec9c
Дмитрий Кучеренко <dmitriyk1689@yandex.ru>
2025-09-07 19:15:40 +0500

    Реализован CRUD для задач, созданы шаблоны

--- config/config/urls.py
+++ config/config/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/signup/', SignUpView.as_view(), name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('tasks/', include('tasks.urls')),
 ]
--- config/tasks/views.py
+++ config/tasks/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from tasks.models import Habit, Task
+
+
+class TaskListView(LoginRequiredMixin, ListView):
+    """Представление для отображения задач."""
+
+    model = Task
+    template_name = 'tasks/task_list.html'
+    context_object_name = 'tasks'
+    paginate_by = 8
+
+    def get_queryset(self):
+        return Task.objects.filter(author=self.request.user)
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    """Представление для создания задачи."""
+
+    model = Task
+    template_name = 'tasks/task_form.html'
+    fields = ['title', 'description']
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    """Представление для редактирования задачи."""
+
+    model = Task
+    template_name = 'tasks/task_form.html'
+    fields = ['title', 'description', 'is_completed']
+    success_url = reverse_lazy('tasks')
+
+    def get_queryset(self):
+        return Task.objects.filter(author=self.request.user)
+
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    """Представление для удаления задачи."""
+
+    model = Task
+    template_name = 'tasks/task_confirm_delete.html'
+    success_url = reverse_lazy('tasks')
+
+    def get_queryset(self):
+        return Task.objects.filter(author=self.request.user)
